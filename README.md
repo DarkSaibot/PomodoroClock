e988b1f8a130dafafa981066f2fa6ec4e7538879
Gabriel Tobias <gabrieltobias10@outlook.com>
2023-06-28 13:37:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pomodoro clock
+A pomodoro clock app made in Fluter/Dart by me and is fully functional.
+<br>
+<br>
+![Screenshot_2](https://github.com/DarkSaibot/Meals/assets/86971123/9741242a-d7c5-4cc6-af94-1039aee047f8)
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/pages/pomodoro.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Contador',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Pomodoro(),
+      ),
+    );
+  }
+}
+
+// final store = ContadorStore();
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Contador'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'Você apertou o botão esse número de vezes:',
+//             ),
+//             Observer(
+//               builder: (_) => Text(
+//                 '${store.contador}',
+//                 style: Theme.of(context).textTheme.headlineMedium,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: store.incrementar,
+//         tooltip: 'Incrementar',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
